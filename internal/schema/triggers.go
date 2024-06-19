6915a9f2b604f600da38a1f8a2459cb25858f64b
Peter Downs <github@peterdowns.com>
2024-06-19 13:14:27 -0400

    bugfix: collision between triggers with the same name

--- internal/schema/triggers.go
+++ internal/schema/triggers.go
@@ -17,7 +17,8 @@ type Trigger struct {
 }
 
 func (t Trigger) SortKey() string {
-	return t.Name
+	// Triggers on different tables may have the same name
+	return identifier(t.TableName, t.Name)
 }
 
 func (t Trigger) DependsOn() []string {
